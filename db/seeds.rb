59c4340dc4df56a91acd77a532e10f37bea15f1f
Gillian Lu <120929023+GillianLu@users.noreply.github.com>
2024-05-01 14:05:39 +0800

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create!(
+  email: 'acostayuukichi@gmail.com',
+  password: '123456',
+  first_name: 'Yuukichi',
+  last_name: 'Acosta',
+  address: 'test test',
+  role: 'admin',
+  confirmed_at: Time.now 
+)
