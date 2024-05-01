e25ad57394b15ccf3e645345685fc38b80af56a0
Gillian Lu <120929023+GillianLu@users.noreply.github.com>
2024-05-01 14:37:20 +0800

    Create $RAILS_ENV.rb

---
+++ config/environments/$RAILS_ENV.rb
@@ -0,0 +1,21 @@
+Rails.application.configure do
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:         'smtp.gmail.com',
+    port:            587,
+    domain:          'stock-trading-8dja.onrender.com', 
+    user_name:       'acostayuukichi@gmail.com',
+    password:        'tanginamo071303', 
+    authentication:  'plain',
+    enable_starttls: true,
+    open_timeout:    5,
+    read_timeout:    5
+  }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: 'no-reply@example.com' }
+
+end
