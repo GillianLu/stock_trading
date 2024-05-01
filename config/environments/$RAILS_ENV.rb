aae704772291296d25bd26f6135498a6eb51a3fd
Gillian Lu <120929023+GillianLu@users.noreply.github.com>
2024-05-01 14:38:55 +0800

    Update $RAILS_ENV.rb

--- config/environments/$RAILS_ENV.rb
+++ config/environments/$RAILS_ENV.rb
@@ -16,6 +16,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = { from: 'no-reply@example.com' }
+  config.action_mailer.default_url_options = { host: 'https://stock-trading-8dja.onrender.com', protocol: 'https' }
+  config.action_mailer.default_options = { from: 'no-reply@stock-trading.com' }
 
 end
