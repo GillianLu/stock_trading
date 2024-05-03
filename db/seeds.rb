4367f6aee7cbe663ee4e4dd959960690d8ec8490
GillianLu <lu.gillian.nicole@gmail.com>
2024-05-03 19:06:24 +0800

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-User.find_or_create_by(email: 'acostayuukichi@gmail.com') do |user|
-  user.password = '1234567890'
-  user.first_name = 'Yuukichi'
-  user.last_name = 'Acosta'
-  user.address = 'test test'
-  user.role = 'admin'
-  user.confirmed_at = Time.now
-end
+# User.find_or_create_by(email: 'acostayuukichi@gmail.com') do |user|
+#   user.password = '1234567890'
+#   user.first_name = 'Yuukichi'
+#   user.last_name = 'Acosta'
+#   user.address = 'test test'
+#   user.role = 'admin'
+#   user.confirmed_at = Time.now
+# end
 
-User.find_or_create_by(email: 'lu.gillian.nicole@gmail.com') do |user|
-  user.password = '123456'
-  user.first_name = 'Gillian'
-  user.last_name = 'Lu'
-  user.address = 'test test'
-  user.role = 'admin'
-  user.confirmed_at = Time.now
-end
+user = User.find_or_initialize_by(email: 'lu.gillian.nicole@gmail.com')
+
+user.password = '123456' unless user.persisted?
+user.first_name = 'Gillian'
+user.last_name = 'Lu'
+user.address = 'test test'
+user.role = 'admin'
+user.confirmed_at = Time.now unless user.confirmed_at
+
+user.save!
